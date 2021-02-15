0282f2b77b9690a666528debc518d7ff4f8810f3
Lucas <lmachado72@outlook.com>
2021-02-15 15:56:11 -0300

    Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 
+import FirstAndSecond from './components/FirstAndSecond';
+
+import GlobalStyles from './styles/global';
+import { Main, Section, Sticky } from './styles';
+
+
 function App() {
   return (
-    <h1>Teste</h1>
+    <>
+      <GlobalStyles />
+
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: '9.7%' }}>
+            <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
+    </>
   );
 }
 
